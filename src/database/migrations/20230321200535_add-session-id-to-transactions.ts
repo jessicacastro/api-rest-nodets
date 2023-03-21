e39909922faf4fb58e593af4258c15fb8fcb02ee
Jessica Castro <jessica.castros@outlook.com>
2023-03-21 17:23:18 -0300

    [core] add dotenv and other knex configs

---
+++ src/database/migrations/20230321200535_add-session-id-to-transactions.ts
@@ -0,0 +1,17 @@
+import { Knex } from 'knex'
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('transactions', (table) => {
+    table.uuid('session_id').after('id').index()
+    // .references('id')
+    // .inTable('sessions')
+    // .onDelete('CASCADE')
+    // .onUpdate('CASCADE')
+  })
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('transactions', (table) => {
+    table.dropColumn('session_id')
+  })
+}
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,34 @@
+import 'dotenv/config'
+import crypto from 'node:crypto'
 import fastify from 'fastify'
+import { knex } from './database/database'
 
 const server = fastify()
+const PORT = Number(process.env.PORT) || 3333
 
 server.get('/isUp', async (request, reply) => {
+  const tables = await knex('sqlite_schema').select('*')
+
   return {
     message: 'Server is up! Start coding and thanks for use this API! 🍾',
+    tables,
   }
+})
+
+server.get('/', async (request, reply) => {
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 100,
+    })
+    .returning('*')
+
+  return transaction
 })
 
 server
   .listen({
-    port: 3333,
+    port: PORT,
   })
-  .then(() => console.log('Server is running at http://localhost:3333 🚀'))
+  .then(() => console.log(`Server is running at http://localhost:${PORT} 🚀`))
